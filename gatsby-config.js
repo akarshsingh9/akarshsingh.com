6fa63f641c28f99bafedb3edfb730e8db87df960
unknown <akarsh.singh9@gmail.com>
2020-07-05 00:33:58 +0530

    favicon added, default image assets removed, Foundation replaced

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,13 +79,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Akarsh Singh`,
+        short_name: `Akarsh`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `static/assets/stackrole.png`,
+        icon: `static/assets/favicon/android-chrome-512x512.png`,
       },
     },
     'gatsby-plugin-offline',
