6fda3409198d8309bb27fcf5c54d96ae9c54d217
unknown <akarsh.singh9@gmail.com>
2020-07-24 00:08:28 +0530

    Portfolio page mobile - post card space issue fix

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,67 +10,79 @@ const Portfolio = () => (
   <h1>Portfolio</h1>
   <div className="grids col-1 sm-2 lg-3">
   
-  <article className="post-card">
-      <h3>akarshsingh.com website</h3>
-      <p>Personal website developed using Gatsby JS, React, JavaScript, CSS. I am currently customizing stackrole's Foundation theme. Using the theme has helped 
+  <article className="post-card"> 
+    <div className="post-content">
+      <div className="title">akarshsingh.com website</div>
+      <p>
+        Personal website developed using Gatsby JS, React, JavaScript, CSS. I am currently customizing stackrole's Foundation theme. Using the theme has helped 
         me get started quickly with Gatsby and learn how to design websites.
       </p>
-      <br/>
       <div>        
         <Link to="https://github.com/akarshsingh9/akarshsingh.com">
         <span className="icon -left"><FaGithubAlt/></span> Source Code
         </Link>
-      </div>      
+      </div>
+      </div>            
   </article>
 
   <article className="post-card">
-      <h3>Code in Python Android App</h3>
-      <p>My first android app made using Java, XML, HTML, JS. I learnt complete development cycle of android app. Published it on Google Play Store.
-      Its completely offline, has quiz, tutorials and glossary related to Python.</p>
-      <br/>
+  <div className="post-content">
+      <div className="title">Code in Python Android App</div>
+      <p>
+        My first android app made using Java, XML, HTML, JS. I learnt complete development cycle of android app. Published it on Google Play Store.
+      Its completely offline, has quiz, tutorials and glossary related to Python.
+      </p>
       <div>        
         <Link to="https://github.com/akarshsingh9/CodeInPython">
         <span className="icon -left"><FaGithubAlt/></span> Source Code
         </Link>
-      </div>      
+      </div>
+  </div>          
   </article>
   
   <article className="post-card">
-      <h3>Materialize Blog Website</h3>
-      <p>My first website theme made using HTML, Materialize CSS, and JavaScript. This website has homepage, blog list section, blog page, disqus integration, author section,
-         twitter and github follow buttons for author, share post on social media.</p>
-      <br/>
+  <div className="post-content">
+      <div className="title">Materialize Blog Website</div>
+      <p>
+        My first website theme made using HTML, Materialize CSS, and JavaScript. This website has homepage, blog list section, blog page, disqus integration, author section,
+         twitter and github follow buttons for author, share post on social media.
+      </p>
       <div>        
         <Link to="https://github.com/akarshsingh9/Materialize-Blog-website">
         <span className="icon -left"><FaGithubAlt/></span> Source Code
         </Link>
-      </div>      
+      </div>
+  </div>          
   </article>
   
   <article className="post-card">
-      <h3>Tour Itinerary Android App</h3>
-      <p>Android app developed from scratch for Hindustan Petroleum Officers. I was in one month internship with HPCL as a Project Trainee. This application helps employees to
+  <div className="post-content">
+      <div className="title">Tour Itinerary Android App</div>
+      <p>
+        Android app developed from scratch for Hindustan Petroleum Officers. I was in one month internship with HPCL as a Project Trainee. This application helps employees to
         plan their tour itinerary for any meetings, training, etc.
       </p>
-      <br/>
       <div>        
         <Link to="https://github.com/akarshsingh9/TourItinerary">
         <span className="icon -left"><FaGithubAlt/></span> Source Code
         </Link>
-      </div>      
+      </div>
+  </div>
   </article>
 
   <article className="post-card">
-      <h3>Flight Search Android App</h3>
-      <p>Android app developed from scratch for OneDirect Interview process. I have prepared this app in less than 24 hours. This app has Listing page for all available flights,
+  <div className="post-content">
+      <div className="title">Flight Search Android App</div>
+      <p>
+        Android app developed from scratch for OneDirect Interview process. I have prepared this app in less than 24 hours. This app has Listing page for all available flights,
          a search option and basic booking screen.
       </p>
-      <br/>
       <div>        
         <Link to="https://github.com/akarshsingh9/FlightSearch">
         <span className="icon -left"><FaGithubAlt/></span> Source Code
         </Link>
       </div>      
+  </div>    
   </article>
 
   </div>
